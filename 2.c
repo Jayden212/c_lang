6752df39770769a956f14658e48ad5f7bfb5997c
Seungjae Lee <jayden212@naver.com>
2022-01-29 23:14:32 +0900

    변수 선언, 출력

---
+++ 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char name[] = "Seungjae";
+	int age = 17;
+    char fvle = 'S';
+	double weight = 55.5;
+	
+	
+printf("His name is %s.\nHe's %d years old.\nHis favorite letter is %c.\nHis weight is %lf kilograms.",name,age,fvle,weight);
+}
